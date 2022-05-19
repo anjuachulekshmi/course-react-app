8898f9db65fb33cd106a8466f58e92f14723bd4a
anjuachulekshmi <anjuachulekshmi836@gmail.com>
2022-05-19 06:04:20 +0530

    edited

--- src/Component/Addcourse.js
+++ src/Component/Addcourse.js
@@ -1,6 +1,22 @@
 import React from 'react'
 
+
+
 const Addcourse = () => {
+
+    
+        var [title,setTitle]=useState("")
+        var [duration,setDuration]=useState("")
+        var [description,setDescription]=useState("")
+        var [venue,setVenue]=useState("")
+        var [date,setDate]=useState("")
+        
+         const read=()=>{
+            var data={"title":title,"duration":duration,"description":description,"venue":venue,"date":date}
+            console.log(data)
+        }
+    
+    
   return (
     <div>
         <div className="container">
@@ -9,33 +25,33 @@ const Addcourse = () => {
             <div className="row g-3">
                 <div className="col  col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                     <label for="" className="form-label">Course Title</label>
-                    <input type="text" className="form-control"/>
+                    <input onChange={(e)=>{setTitle(e.target.value)}} type="text" className="form-control"/>
                 </div>
                
                 <div className="col  col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                     <label for="" className="form-label">Duration</label>
-                    <input type="text" className="form-control"/>
+                    <input onChange={(e)=>{setDuration(e.target.value)}} type="text" className="form-control"/>
                 </div>
                 
                     <div className="col  col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                     <label for="" className="form-label">Description</label>
-                    <input type="text" className="form-control"/>
+                    <input onChange={(e)=>{setDescription(e.target.value)}} type="text" className="form-control"/>
 
                 </div>
 
                 <div className="col  col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                     <label for="" className="form-label">Venue</label>
-                    <input type="text" className="form-control"/>
+                    <input onChange={(e)=>{setVenue(e.target.value)}} type="text" className="form-control"/>
                     </div>
 
                     <div className="col  col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                     <label for="" className="form-label">Date</label>
-                    <input type="text" className="form-control"/>
+                    <input onChange={(e)=>{setDate(e.target.value)}} type="text" className="form-control"/>
                     </div>
                
                
                 <div className="col  col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                <button className="btn btn-success">REGISTER</button>
+                <button onClick={read} className="btn btn-success">REGISTER</button>
                 </div>
             </div>
         </div>
